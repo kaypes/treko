f5ff6a815385ebde8445481c72a62a60a165b866
Kayque Pereira <114672278+jokayque@users.noreply.github.com>
2025-12-21 13:59:26 -0300

    Adds token and token type definitions
    Defines the basic structure for tokens, including its type, lexeme, literal value, and line number.
    
    Introduces an enumeration for the different types of tokens the scanner will recognize.

---
+++ Sources/treko/TokenType.swift
@@ -0,0 +1,13 @@
+enum TokenType {
+    case leftParen, rightParen, leftBrace, rightBrace
+    case comma, dot, semicolon
+    case minus, plus, slash, star
+    case equal, equalEqual, bang, bangEqual, greater, greaterEqual, less, lessEqual
+    case identifier, string, number, bool, `true`, `false`, `nil`
+    case and, or, `if`, elsif, `else`, unless
+    case `class`, this, `super`
+    case `var`, `let`
+    case fun, `print`, `return`
+    case loop, `while`, `for`, until
+    case eof
+}
